308995d0851ff66096950e95fae098e47a0d5a83
Tanweer Kamal <tanweerkamal54@gmail.com>
2023-10-02 09:45:02 +0530

    Added init folder

---
+++ init/data.js
@@ -0,0 +1,92 @@
+    const historicalQuotes = [
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Albert_Einstein_Head.jpg",
+        name: "Albert Einstein",
+        quote: "Imagination is more important than knowledge."
+    },
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/7/7a/Mahatma-Gandhi%2C_studio%2C_1931.jpg",
+        name: "Mahatma Gandhi",
+        quote: "Be the change that you wish to see in the world."
+    },
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Abraham_Lincoln_O-77_matte_collodion_print.jpg/1200px-Abraham_Lincoln_O-77_matte_collodion_print.jpg",
+        name: "Abraham Lincoln",
+        quote: "In the end, it's not the years in your life that count; it's the life in your years."
+    },
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/0/05/Martin_Luther_King%2C_Jr..jpg",
+        name: "Martin Luther King Jr.",
+        quote: "Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that."
+    },
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Sir_Winston_Churchill_-_19086236948.jpg/800px-Sir_Winston_Churchill_-_19086236948.jpg",
+        name: "Winston Churchill",
+        quote: "Success is not final, failure is not fatal: It is the courage to continue that counts."
+    },
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Nelson_Mandela_1994.jpg/1200px-Nelson_Mandela_1994.jpg",
+        name: "Nelson Mandela",
+        quote: "Education is the most powerful weapon which you can use to change the world."
+    },
+    {
+        image: "https://assets.editorial.aetnd.com/uploads/2012/05/self-portrait-by-leonardo-da-vinci.jpg",
+        name: "Leonardo da Vinci",
+        quote: "Simplicity is the ultimate sophistication."
+    },
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Marie_Curie_c._1920s.jpg/1200px-Marie_Curie_c._1920s.jpg",
+        name: "Marie Curie",
+        quote: "Nothing in life is to be feared; it is only to be understood."
+    },
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/John_F._Kennedy%2C_White_House_color_photo_portrait.jpg/800px-John_F._Kennedy%2C_White_House_color_photo_portrait.jpg",
+        name: "John F. Kennedy",
+        quote: "Ask not what your country can do for you; ask what you can do for your country."
+    },
+    {
+        image: "https://m.media-amazon.com/images/M/MV5BMjIxMTUyNjc5OF5BMl5BanBnXkFtZTcwMjMwNjYwNA@@._V1_FMjpg_UX1000_.jpg",
+        name: "Walt Disney",
+        quote: "All our dreams can come true if we have the courage to pursue them."
+    },
+    {
+        image: "https://hips.hearstapps.com/hmg-prod/images/gettyimages-79035252.jpg",
+        name: "Charles Darwin",
+        quote: "It is not the strongest of the species that survives, nor the most intelligent; it is the one most responsive to change."
+    },
+    {
+        image: "https://cdn.britannica.com/72/142272-050-12F9270F/Helen-Keller-1904.jpg",
+        name: "Helen Keller",
+        quote: "The only thing worse than being blind is having sight but no vision."
+    },
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Thomas_Edison2.jpg/800px-Thomas_Edison2.jpg",
+        name: "Thomas Edison",
+        quote: "I have not failed. I've just found 10,000 ways that won't work."
+    },
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Mark_Twain_by_AF_Bradley.jpg/640px-Mark_Twain_by_AF_Bradley.jpg",
+        name: "Mark Twain",
+        quote: "The secret of getting ahead is getting started."
+    },
+    {
+        image: "https://cdn.britannica.com/82/93182-050-E03416A7/Marilyn-Monroe.jpg",
+        name: "Marilyn Monroe",
+        quote: "Imperfection is beauty, madness is genius, and it's better to be absolutely ridiculous than absolutely boring."
+    },
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/c/c4/Rosaparks.jpg",
+        name: "Rosa Parks",
+        quote: "The only tired I was, was tired of giving in."
+    },
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/d/dc/Steve_Jobs_Headshot_2010-CROP_%28cropped_2%29.jpg",
+        name: "Steve Jobs",
+        quote: "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work."
+    },
+    {
+        image: "https://cdn.britannica.com/63/59963-050-C03F29B9/Pablo-Picasso.jpg",
+        name: "Pablo Picasso",
+        quote: "Everything you can imagine is real."
+    },
+];
